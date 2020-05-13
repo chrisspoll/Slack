0ddf8f42c24d937501b4d241683ec9adf7d050c5
chrisspoll <62038377+chrisspoll@users.noreply.github.com>
2020-05-13 17:00:36 +0200

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,5 +4,12 @@ public class CreateChannel {
 
     public static void main(String[] args) {
         System.out.println("Hello from Slack project");
+        
+        String s= "a";
+        if(s.contains("a")){
+            System.out.println("contains");
+        }else{
+          System.out.println("not");
+        }
     }
 }
