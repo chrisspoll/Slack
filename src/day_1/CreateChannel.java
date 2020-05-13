7989f1259627b76a028bd6852e62ad32eb808ce2
chrisspoll <62038377+chrisspoll@users.noreply.github.com>
2020-05-13 17:01:43 +0200

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -5,7 +5,7 @@ public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("Hello from Slack project");
         
-        String s= "a";
+        String s= "a"; //GitHub
         if(s.contains("a")){
             System.out.println("contains");
         }else{
